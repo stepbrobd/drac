0fa94b947e650265016c7b48608d004dac3845f2
Yifei Sun <ysun@hey.com>
2026-06-07 17:27:40 +0200

    modules/flake/crane: follow crane workspace docs

--- modules/flake/crane.nix
+++ modules/flake/crane.nix
@@ -1,13 +1,40 @@
-{ inputs, ... }:
+{ inputs, self, ... }:
 
 {
-  perSystem = { crane, pkgs, ... }: {
+  perSystem = { crane, lib, pkgs, ... }: {
     _module.args.crane = {
       lib = inputs.crane.mkLib pkgs;
 
-      src = crane.lib.cleanCargoSource inputs.self.outPath;
+      src = crane.lib.cleanCargoSource self.outPath;
 
-      # TODO: add common crate args here?
+      commonArgs = {
+        inherit (crane) src;
+        strictDeps = true;
+        __structuredAttrs = true;
+
+        # crane cant infer pname/version
+        # set a placeholder and override in per crate drv
+        pname = "drac";
+        version = "2001.717.0";
+      };
+
+      # pre-build/cache deps
+      cargoArtifacts = crane.lib.buildDepsOnly crane.commonArgs;
+
+      individualCrateArgs = crane.commonArgs // {
+        inherit (crane) cargoArtifacts;
+        # test with cargo-nextest
+        doCheck = false;
+      };
+
+      fileSetForCrates = crates: lib.fileset.toSource {
+        root = ../..;
+
+        fileset = lib.fileset.unions ([
+          ../../Cargo.toml
+          ../../Cargo.lock
+        ] ++ lib.map crane.lib.fileset.commonCargoSources crates);
+      };
     };
   };
 }
