6ac29f08430ba6909421b49fc0a4ced226d8b285
Yifei Sun <ysun@hey.com>
2026-09-13 23:48:49 +0200

    modules/flake: put kani and a fuzz wrapper in the dev shell

--- modules/flake/shell.nix
+++ modules/flake/shell.nix
@@ -2,22 +2,33 @@
 # SPDX-License-Identifier: Apache-2.0
 
 {
-  perSystem = { crane, pkgs, ... }: {
-    devShells.default = crane.lib.devShell {
-      packages = with pkgs; [
-        # Formatter stuff
-        deno
-        nixpkgs-fmt
-        taplo
+  perSystem = { crane, pkgs, ... }:
+    let
+      # Sanitizer flags are nightly only, and the kani package pins one exactly
+      fuzz = pkgs.writeShellScriptBin "drac-fuzz" ''
+        set -eu
+        export PATH=${pkgs.kani.toolchain}/bin:${pkgs.cargo-fuzz}/bin:$PATH
+        exec cargo fuzz "$@"
+      '';
+    in
+    {
+      devShells.default = crane.lib.devShell {
+        packages = with pkgs; [
+          # Called by nix fmt, see formatter.nix
+          deno
+          nixpkgs-fmt
+          taplo
 
-        # cargo   # from crane
-        # clippy  # from crane
-        # rustc   # from crane
-        # rustfmt # from crane
-        # rust-analyzer # from crane
-        cargo-hakari
-        cargo-nextest
-      ];
+          # cargo   # from crane
+          # clippy  # from crane
+          # rustc   # from crane
+          # rustfmt # from crane
+          # rust-analyzer # from crane
+          cargo-hakari
+          cargo-nextest
+          fuzz
+          kani
+        ];
+      };
     };
-  };
 }
