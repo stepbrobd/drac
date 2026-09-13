43ceed0b8744fbbb43105f34ac9b82a028c2e1bc
Yifei Sun <ysun@hey.com>
2026-09-14 00:36:30 +0200

    modules/flake: read the crate list from the crane module

--- modules/flake/crates.nix
+++ modules/flake/crates.nix
@@ -5,21 +5,17 @@
   perSystem = { crane, lib, ... }: {
     legacyPackages.crates =
       let
-        directories = lib.attrNames (
-          lib.filterAttrs
-            (_: type: type == "directory")
-            (lib.readDir ../../crates));
-
         override = crate:
           let file = ../../crates/${crate}/crane.nix;
           in if lib.pathExists file then import file else { };
       in
-      # Force export cargo deps, i.e. there must NOT be a crate called drac-deps
+      # Force export cargo deps
+      assert !(lib.elem "drac-deps" crane.crateDirs);
       { drac-deps = crane.cargoArtifacts; }
       //
       lib.genAttrs
         # Drop crates w/ { disable = true; }
-        (lib.filter (crate: !((override crate).disable or false)) directories)
+        (lib.filter (crate: !((override crate).disable or false)) crane.crateDirs)
         (crate: crane.builder crate (lib.removeAttrs (override crate) [ "disable" ]));
   };
 }
